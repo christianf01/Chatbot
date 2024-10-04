2b5accf4c648f2f44213ce1c83cc6f9ef59fe5ee
christianf01 <128834310+christianf01@users.noreply.github.com>
2024-10-03 22:59:12 -0500

    Update core.py

--- chatbot/core.py
+++ chatbot/core.py
@@ -1,1 +1,19 @@
+from .vector_search import VectorSearch
+from .text_retrieval import TextRetrieval
 
+class KnowledgeFlow:
+    def __init__(self, index_type, dimension, vectors, texts, URI, db_name, collection_name):
+        self.vector_search = VectorSearch(index_type, dimension)
+        initial_vector_ids = self.vector_search.create_index(vectors)
+        self.text_retrieval = TextRetrieval(URI, db_name, collection_name, initial_vector_ids, texts)
+
+    def search(self, query_vector, k):
+        distances, indices = self.vector_search.search(query_vector, k)
+        
+        texts = [self.text_retrieval.get_text(index) for index in indices]
+        return texts
+    
+    # add texts
+    def add(self, vectors, texts):
+        vector_ids = self.vector_search.add_vectors(vectors)
+        self.text_retrieval.add_texts(vector_ids, texts)
